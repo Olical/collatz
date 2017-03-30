09f5d8771b218504b1fc43c0b2dec1a3eee43845
Oliver Caldwell <olliec87@gmail.com>
2017-03-30 23:20:35 +0100

    Make the quil part simpler

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject collatz "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Collatz conjecture computation with snazzy rendering."
+  :url "https://github.com/Olical/collatz"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [quil "2.6.0"]])
+                 [quil "2.6.0"]]
+  :main collatz.core)
